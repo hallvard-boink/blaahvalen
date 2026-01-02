4eb84f91dcbfd58efef0989ddcc307164792a042
hallvard-boink <hl.interests.hobbies.sports@tutamail.com>
2026-01-02 07:47:42 +0100

    blaahvalen-18 Månedsoversikt: kostnadspakker på utskriften

---
+++ src/main/java/com/hallvardlaerum/periodepost/periodeoversiktpost/PeriodedelAvKostnadspakkeRad.java
@@ -0,0 +1,46 @@
+package com.hallvardlaerum.periodepost.periodeoversiktpost;
+
+import com.hallvardlaerum.periodepost.Periodepost;
+
+public class PeriodedelAvKostnadspakkeRad {
+    private Periodepost kostnadspakke;
+    private Integer sumForDenneMaaned;
+
+
+    public PeriodedelAvKostnadspakkeRad(Periodepost kostnadspakke, Integer sumForDenneMaaned) {
+        this.kostnadspakke = kostnadspakke;
+        this.sumForDenneMaaned = sumForDenneMaaned;
+    }
+
+    public Periodepost getKostnadspakke() {
+        return kostnadspakke;
+    }
+
+    public void setKostnadspakke(Periodepost kostnadspakke) {
+        this.kostnadspakke = kostnadspakke;
+    }
+
+    public Integer getSumForDenneMaaned() {
+        return sumForDenneMaaned;
+    }
+
+    public void setSumForDenneMaaned(Integer sumForDenneMaaned) {
+        this.sumForDenneMaaned = sumForDenneMaaned;
+    }
+
+    public Integer getSumTotalt() {
+        if (kostnadspakke == null) {
+            return 0;
+        } else {
+            return kostnadspakke.getSumRegnskapInteger();
+        }
+    }
+
+    public String getTittel() {
+        if (kostnadspakke == null) {
+            return "";
+        } else {
+            return kostnadspakke.getTittelString();
+        }
+    }
+}
